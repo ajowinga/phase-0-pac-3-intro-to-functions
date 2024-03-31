0ea902d9c2377cc51fe2acdfcb3e512df7a78829
ajowinga <joanwinga1@gmail.com>
2024-03-31 07:30:52 +0300

    attempted

--- index.js
+++ index.js
@@ -1,1 +1,127 @@
-// Follow along with the examples here
+// function sayHello() {
+//   console.log("Hello!");
+// }
+// sayHello();
+
+// function sayHelloToGuadalupe() {
+//   console.log("Hello, Guadalupe!");
+// }
+// sayHelloToGuadalupe()
+
+// function sayHelloToLiz() {
+//   console.log("Hello, Liz!");
+// }
+// sayHelloToLiz()
+
+// function sayHelloToSamip(){
+//   console.log("Hello,Tamip!");
+// }
+// sayHelloToSamip()
+
+// sayHelloToGuadalupe();
+// sayHelloToLiz();
+// sayHelloToSamip();
+
+// function myFruits(){
+//   console.log("mango,passionfruit,apples")
+// };
+// myFruits()
+
+// function animals(){
+//   console.log("pig,cat,orangutan")
+// };
+// animals()
+
+// function berries(){
+//   console.log("blue,rasp,straw")
+// };
+// berries()
+
+// firstDeclareTheFunction function using the word function followed by name of function
+// function berries()
+
+// add {} thatContainTheCodeToBeExecuted {
+//   console.log("Hello!");
+// }
+// callTheFunction by typingTheNameOfTheFunction 
+// function berries
+
+function doSomething(thing) {
+  console.log(thing);
+}
+
+doSomething("anything");
+
+// function sayHelloTo(firstName) {
+//   console.log(`Hello, ${firstName}!`);
+// }
+
+// sayHelloTo("Guadalupe"); // "Hello, Guadalupe!"
+// sayHelloTo("Jane"); // "Hello, Jane!"
+// sayHelloTo("R2-D2"); // "Hello, R2-D2!"
+// sayHelloTo(1); // "Hello, 1!"
+
+// function fruits(mango,apple,berries){
+//   console.log ("mango,apple,berries")
+// }
+
+// fruits("mango,apple,berries");
+
+// function say(greeting, firstName) {
+//   console.log(`${greeting}, ${firstName}!`);
+// }
+// say("Goodbye", "Julio");
+
+// function say(greeting, firstName) {
+//   console.log("firstName: ", firstName);
+//   console.log("greeting: ", greeting);
+//   console.log(`${greeting}, ${firstName}!`);
+// }
+// say("Goodbye", "Julio");
+
+// function add(x,y){
+//   return x + y;
+// }
+// console.log(add(1, 2));
+
+// function add(x,y){
+//   return x + y;
+// }
+// console.log(add(8000, 90));
+
+
+// function say(greeting, firstName) {
+//   return `${greeting}, ${firstName}!`;
+// }
+// console.log(say("Hello", "Liz"));
+
+// function say(greeting, firstName) {
+//   console.log(`${greeting}, ${firstName}!`);
+// }
+// console.log(say("Hello", "Liz"));
+
+// function add(x, y) {
+//   return x + y;
+// }
+// console.log(add(1, 2));
+// function say(greeting, firstName) {
+//   return `${greeting}, ${firstName}!`;
+//   console.log("I was called!");
+// }
+// console.log(say("Howdy", "partner"));
+
+// function say(greeting, firstName) {
+//   console.log("I was called!");
+//   return `${greeting}, ${firstName}!`;
+// }
+// console.log(say("Howdy", "partner"));
+
+// function add (num1 ,num2){
+//   return `${num1+num2}`
+// }
+// console.log (add(2000,30))
+
+function say( greeting,firstName){
+  console.log(`$(greeting),$(firstName)`) ;
+}
+console.log(say("Hello", "Joan"));
